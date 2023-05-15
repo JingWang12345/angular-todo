b4a86611d7af03633ac1454b8d9a582f2a959dcf
JingWang12345 <127298095+JingWang12345@users.noreply.github.com>
2023-05-15 12:02:37 +0200

    addTodo

---
+++ src/app/components/todo-add/todo-add.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styleUrls: ['./todo-add.component.scss']
+})
+export class TodoAddComponent {
+  newTodo: Todo = {title:'', priority:1, description: ''}
+
+  @Output() todoCreated: EventEmitter<Todo> = new EventEmitter();
+
+
+  saveTodo(){
+  console.log(this.newTodo);
+  this.todoCreated.emit({...this.newTodo});
+
+  }
+}
+
